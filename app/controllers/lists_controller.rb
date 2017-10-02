06b4c15763337a93322b0a0fbad852a6448efeff
Blair Anderson <Blair81@gmail.com>
2017-10-02 16:41:10 -0700

    yolo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_list, only: [:edit, :update, :destroy]
 
   def index
     @lists = List.published.includes(items: :brand).where.not(items: {id: nil})
