f278234f53071dda6a4ba92d7a4bb7ea0e081b17
Blair Anderson <Blair81@gmail.com>
2018-04-12 21:28:41 -0700

    fix this migration

--- db/migrate/20180413002411_make_list_items_more_awesome.rb
+++ db/migrate/20180413002411_make_list_items_more_awesome.rb
@@ -1,7 +1,5 @@
 class MakeListItemsMoreAwesome < ActiveRecord::Migration
   def change
-    # title:string body:text amazon_link:text click_count:integer
-    add_column :list_items, :title, :text
     add_column :list_items, :body, :text
     add_column :list_items, :affiliate_link, :text
     add_column :list_items, :click_count, :integer, default: 0
