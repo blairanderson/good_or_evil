7f14ed76c3f14e29ecdb86d7a89df380998c7e19
Blair Anderson <Blair81@gmail.com>
2018-04-18 09:25:01 -0700

    add fonts and shit

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,10 +2,13 @@
 # should be fast as possible with very good caching
 # no authentication at all
 class LandingController < ApplicationController
+
   def index
+
   end
 
   def started
+  #   THIS IS A SIGNUP REDIRECT FOR CONVERSION CHECKS
   end
 
   def pricing
