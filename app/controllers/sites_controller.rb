db998a7e5357f3472bd1e9b9e32decb33cb0cc70
Blair Anderson <Blair81@gmail.com>
2018-04-19 07:33:43 -0700

    yolo

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,12 @@ class SitesController < PublicController
   layout "site"
 
   def index
-    @lists = current_account.lists.published.paginate(per_page: current_account.lists_per_page, page: params[:page])
+    lists = current_account.lists.published
+    @query = params[:q].to_s.strip
+    if @query.length > 0
+      lists = lists.search_for(@query)
+    end
+    @lists = lists.paginate(per_page: current_account.lists_per_page, page: params[:page])
     current_account.update_column(:page_views, current_account.page_views+1)
   end
 
@@ -13,7 +18,7 @@ class SitesController < PublicController
   helper_method :current_list
 
   def show
-    current_list.update_column(:page_views, @list.page_views+1)
-    session[:historical_list_ids] = (Array(session[:historical_list_ids]).unshift(@list.id)).flatten.uniq.slice(0, 10)
+    session[:historical_list_ids] = (Array(session[:historical_list_ids]).unshift(current_list.id)).flatten.uniq.slice(0, 10)
+    current_list.update_column(:page_views, current_list.page_views+1)
   end
 end
